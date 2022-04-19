6d907bf10bc663e1ff5554cb0152e7727e7ec8e2
Guilherme Regis <guilhermeb.regis@gmail.com>
2022-04-19 15:54:45 -0300

    creates basic app structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,36 @@
 import React from 'react';
+
+import './index.scss';
+
+// Components
 import Add from './components/AddComponent';
+import List from './components/ListComponent';
+import Config from './components/ConfigComponent';
+import Header from './components/HeaderComponent';
+import Footer from './components/FooterComponent';
+import Msg from './components/MsgComponent';
+
+// Images
+import HeaderDarkMobile from './assets/img/bg-mobile-dark.jpg';
+import HeaderDarkDesktop from './assets/img/bg-desktop-dark.jpg';
+import HeaderLightMobile from './assets/img/bg-mobile-light.jpg';
+import HeaderLightDesktop from './assets/img/bg-desktop-light.jpg';
+
+// Icons
+import {BiLoader} from 'react-icons/bi';
 
 const App = () => {
     return (
-        <Add />
+        <>
+        <div className="content">
+            <Header />
+            <Add />
+            <Msg />
+            <List />
+            <Config />
+            <Footer />
+        </div>
+        </>
     )
 };
 
