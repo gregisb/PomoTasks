7d20f820ad5ccc1ad927beee3eb6889713da78dd
Guilherme Regis <guilhermeb.regis@gmail.com>
2022-04-19 18:29:57 -0300

    setting messages

--- src/components/MsgComponent/index.jsx
+++ src/components/MsgComponent/index.jsx
@@ -1,6 +1,36 @@
-import React from 'react'
+import React from 'react';
 
-const Msg = () => {
+import { BsInfoCircle } from 'react-icons/bs';
+
+const Msg = ({tasks, filter, loading}) => {
+  const [message, setMessage] = useState(null);
+
+  useEffect(() => {
+    if (loading) return
+    if (tasks.length > 0) {
+      setMessage(null);
+      return;
+    }
+    if (tasks.length <= 0 && filter === 'all') {
+      setMessage({
+        title: 'No items found',
+        text: "You haven't added any task. Add one!"
+      })
+    }
+    if (tasks.length <= 0 && filter === 'active') {
+      setMessage({
+        title: 'No active tasks found',
+        text: "Great! You don't have active tasks"
+      })
+    }
+    if (tasks.length <= 0 && filter === 'completed') {
+      setMessage({
+        title: 'No completed tasks found',
+        text: "So bad... You need to get back to work!"
+      })
+    }
+  }, [tasks]);
+
   return (
     <div>Msg</div>
   )
