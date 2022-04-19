da3f96f76a16dac92a3ab2556fcad45e71796668
Guilherme Regis <guilhermeb.regis@gmail.com>
2022-04-19 17:13:47 -0300

    successful connection to database

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,10 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 import './index.scss';
+
+// Firestore
+import { collection, deleteDoc, doc, onSnapshot } from "firebase/firestore";
+import store from './firebase/firebase.config';
 
 // Components
 import Add from './components/AddComponent';
@@ -20,6 +24,38 @@ import HeaderLightDesktop from './assets/img/bg-desktop-light.jpg';
 import {BiLoader} from 'react-icons/bi';
 
 const App = () => {
+    const [tasks, setTasks] = useState([]);
+    const [tasksAll, setTasksAll] = useState([]);
+    const [theme, setTheme] = useState('dark');
+    const [id, setId] = useState(null);
+    const [currentFilter, setCurrentFilter] = useState("all");
+    const [reset, setReset] = useState(false);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        onSnapshot(collection(store, "tasks"), (snapshot) => {
+            let temp = [];
+            snapshot.docs.forEach((doc) => {
+                temp.push({ ...doc.data(), id: doc.id });
+            });
+            setLoading(false);
+            setTasks(temp);
+            setTasksAll(temp);
+
+            const completed = temp.filter((task) => task.completed);
+            let arrCompleted = [];
+            completed.forEach((item) => {
+                arrCompleted.push(item.id);
+            });
+            setCompleted(arrCompleted);
+            console.log('console', temp)
+        });
+    }, []);
+
+    const setCompleted = (newId) => console.log(newId);
+    
+
+
     return (
         <>
         <div className="content">
--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -6,7 +6,7 @@ import { getFirestore } from "firebase/firestore";
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_APIKEY,
   authDomain: process.env.REACT_APP_FIREBASE_AUTHDOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECTID,
+  projectId: "pomotasks-429ba",
   storageBucket: process.env.REACT_APP_FIREBASE_STORAGEBUCKET,
   messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGINGSENDERID,
   appId: process.env.REACT_APP_FIREBASE_APPID
