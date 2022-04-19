5e29f6530e8b3f89df7eb27e440186033706166f
Guilherme Regis <guilhermeb.regis@gmail.com>
2022-04-19 17:36:24 -0300

    sets general component structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,73 +1,94 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 
-import './index.scss';
+import "./index.scss";
 
 // Firestore
 import { collection, deleteDoc, doc, onSnapshot } from "firebase/firestore";
-import store from './firebase/firebase.config';
+import store from "./firebase/firebase.config";
 
 // Components
-import Add from './components/AddComponent';
-import List from './components/ListComponent';
-import Config from './components/ConfigComponent';
-import Header from './components/HeaderComponent';
-import Footer from './components/FooterComponent';
-import Msg from './components/MsgComponent';
+import Add from "./components/AddComponent";
+import List from "./components/ListComponent";
+import Config from "./components/ConfigComponent";
+import Header from "./components/HeaderComponent";
+import Footer from "./components/FooterComponent";
+import Msg from "./components/MsgComponent";
 
 // Images
-import HeaderDarkMobile from './assets/img/bg-mobile-dark.jpg';
-import HeaderDarkDesktop from './assets/img/bg-desktop-dark.jpg';
-import HeaderLightMobile from './assets/img/bg-mobile-light.jpg';
-import HeaderLightDesktop from './assets/img/bg-desktop-light.jpg';
+import HeaderDarkMobile from "./assets/img/bg-mobile-dark.jpg";
+import HeaderDarkDesktop from "./assets/img/bg-desktop-dark.jpg";
+import HeaderLightMobile from "./assets/img/bg-mobile-light.jpg";
+import HeaderLightDesktop from "./assets/img/bg-desktop-light.jpg";
 
 // Icons
-import {BiLoader} from 'react-icons/bi';
+import { BiLoader, BiLoaderAlt } from "react-icons/bi";
 
 const App = () => {
-    const [tasks, setTasks] = useState([]);
-    const [tasksAll, setTasksAll] = useState([]);
-    const [theme, setTheme] = useState('dark');
-    const [id, setId] = useState(null);
-    const [currentFilter, setCurrentFilter] = useState("all");
-    const [reset, setReset] = useState(false);
-    const [loading, setLoading] = useState(true);
+  const [tasks, setTasks] = useState([]);
+  const [tasksAll, setTasksAll] = useState([]);
+  const [theme, setTheme] = useState("dark");
+  const [id, setId] = useState(null);
+  const [currentFilter, setCurrentFilter] = useState("all");
+  const [Reset, setReset] = useState(false);
+  const [loading, setLoading] = useState(true);
 
-    useEffect(() => {
-        onSnapshot(collection(store, "tasks"), (snapshot) => {
-            let temp = [];
-            snapshot.docs.forEach((doc) => {
-                temp.push({ ...doc.data(), id: doc.id });
-            });
-            setLoading(false);
-            setTasks(temp);
-            setTasksAll(temp);
+  useEffect(() => {
+    onSnapshot(collection(store, "tasks"), (snapshot) => {
+      let temp = [];
+      snapshot.docs.forEach((doc) => {
+        temp.push({ ...doc.data(), id: doc.id });
+      });
+      setLoading(false);
+      setTasks(temp);
+      setTasksAll(temp);
 
-            const completed = temp.filter((task) => task.completed);
-            let arrCompleted = [];
-            completed.forEach((item) => {
-                arrCompleted.push(item.id);
-            });
-            setCompleted(arrCompleted);
-            console.log('console', temp)
-        });
-    }, []);
+      const completed = temp.filter((task) => task.completed);
+      let arrCompleted = [];
+      completed.forEach((item) => {
+        arrCompleted.push(item.id);
+      });
+      setCompleted(arrCompleted);
+      console.log("console", temp);
+    });
+  }, []);
 
-    const setCompleted = (newId) => console.log(newId);
-    
+  const setCompleted = (newId) => setId(newId);
+  const changeTheme = (newTheme) => setTheme(newTheme);
+  const getAllTasks = () => (setTasks(tasksAll), setCurrentFilter("all"));
+  const getActiveTasks = (activeTasks) => (
+    setTasks(activeTasks), setCurrentFilter("active")
+  );
+  const getCompletedTasks = (completedTasks) => (
+    setTasks(completedTasks), setCurrentFilter("completed")
+  );
+  const reset = (isReset) => setReset(isReset);
 
-
-    return (
-        <>
-        <div className="content">
-            <Header />
-            <Add />
-            <Msg />
-            <List />
-            <Config />
-            <Footer />
-        </div>
-        </>
-    )
+  return (
+    <>
+      <div className={"content " + theme}>
+        <Header changeTheme={changeTheme} />
+        <Add countTask={tasks.length} setReset={reset} reset={Reset} />
+        {loading ? (
+          <div className="loading">
+            <h2>Loading...</h2>
+            <BiLoaderAlt className="icon-loading" />
+          </div>
+        ) : null}
+        <Msg tasks={tasks} filter={currentFilter} loading={loading} />
+        <List list={tasks} />
+        <Config
+          numTasks={tasks.length}
+          completed={id}
+          staticTasks={tasksAll}
+          getAll={getAllTasks}
+          getActive={getActiveTasks}
+          getCompleted={getCompletedTasks}
+          reset={Reset}
+        />
+        <Footer />
+      </div>
+    </>
+  );
 };
 
 export default App;
