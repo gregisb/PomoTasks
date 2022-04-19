635d6a84d0cd07c3efacaba6f84aa8c92fed950a
Guilherme Regis <guilhermeb.regis@gmail.com>
2022-04-19 18:21:09 -0300

    stting add component styles

--- src/components/AddComponent/index.jsx
+++ src/components/AddComponent/index.jsx
@@ -1,8 +1,33 @@
-import React from 'react'
+import React from 'react';
 
-const Add = () => {
+import store from '../../firebase/firebase.config';
+import { collection, addDoc } from 'firebase/firestore';
+
+const Add = ({ countTask, setReset, reset }) => {
+  const addTask = async (e) => {
+    e.preventDefault();
+    if (e.target[0].value === '') return
+    const docRef = await addDoc(collection(store, 'tasks '), {
+      index: countTask + 1,
+      task: e.target[0].value,
+      completed: false,
+    });
+    e.target.value = '';
+    setReset(!reset);
+  }
+
   return (
-    <div>Add</div>
+    <form className="add-task" onSubmit={addTask}>
+      <label htmlFor="task"></label>
+      <input 
+          type="text" 
+          id="task" 
+          placeholder='Create a new task...' 
+          autoFocus 
+          autoComplete='off' 
+          required 
+      />
+    </form>
   )
 }
 
