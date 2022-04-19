3cb1644b1b0b621542d635faed631ff06f39b378
Guilherme Regis <guilhermeb.regis@gmail.com>
2022-04-19 16:01:16 -0300

    sets firebase config

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyD4xRtK1Wl4QoIFukRhkjWjDhQ_pjBDXE8",
+  authDomain: "pomotasks-429ba.firebaseapp.com",
+  projectId: "pomotasks-429ba",
+  storageBucket: "pomotasks-429ba.appspot.com",
+  messagingSenderId: "710238811809",
+  appId: "1:710238811809:web:e81987311a6562eb0022e0"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
