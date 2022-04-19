34c09f5f8f0f02e7f1e683adb597a768a27d50cf
Guilherme Regis <guilhermeb.regis@gmail.com>
2022-04-19 20:18:08 -0300

    set styles for list and fixes some bugs

--- src/components/AddComponent/index.jsx
+++ src/components/AddComponent/index.jsx
@@ -4,30 +4,30 @@ import store from '../../firebase/firebase.config';
 import { collection, addDoc } from 'firebase/firestore';
 
 const Add = ({ countTask, setReset, reset }) => {
-  const addTask = async (e) => {
-    e.preventDefault();
-    if (e.target[0].value === '') return
-    const docRef = await addDoc(collection(store, 'tasks '), {
-      index: countTask + 1,
-      task: e.target[0].value,
-      completed: false,
-    });
-    e.target.value = '';
-    setReset(!reset);
-  }
 
+  const addTask = async (e) => {
+      e.preventDefault();
+      if(e.target[0].value === "") {
+          return;
+      }
+      const docRef = await addDoc(collection(store, "tasks"), {
+          index: countTask + 1,
+          task: e.target[0].value,
+          completed: false,
+      });
+      e.target[0].value = "";
+      setReset(!reset);
+  }
   return (
-    <form className="add-task" onSubmit={addTask}>
-      <label htmlFor="task"></label>
-      <input 
-          type="text" 
-          id="task" 
-          placeholder='Create a new task...' 
-          autoFocus 
-          autoComplete='off' 
-          required 
-      />
-    </form>
+      <form className="add-task" onSubmit={addTask}>
+          <label htmlFor="task"></label>
+          <input
+              type="text"
+              id="task"
+              placeholder="Create a new task..."
+              autoFocus autoComplete="off"
+              required />
+      </form>
   )
 }
 
--- src/components/ListComponent/index.jsx
+++ src/components/ListComponent/index.jsx
@@ -7,39 +7,83 @@ import { VscChromeClose } from 'react-icons/vsc';
 import { deleteDoc, updateDoc, doc } from 'firebase/firestore';
 import store from '../../firebase/firebase.config';
 
-const List = () => {
-  const [list, setList] = useState(null);
-  const [completed, setCompleted] = useState([]);
-
+const List = ({ list }) => {
+  const [List, setList] = useState(null);
+  const [Completed, setCompleted] = useState([]);
+  
   useEffect(() => {
-    setList(list);
+      setList(list);
   }, [list]);
 
   const handleDrag = (result) => {
-    const { source, destination } = result;
-    if (!destination) return;
-    if (destination.index === source.index && destination.droppableId === source.droppableId) return;
-    setList(list => reorder(list, source.index, destination.index));
-  };
+      const { source, destination } = result;
+      if (!destination) return;
+      if (destination.index === source.index
+          && destination.droppableId === source.droppableId) return;
+      setList(List => reorder(List, source.index, destination.index));
+  }
 
   const reorder = (list, sourceIndex, destinationIndex) => {
-    const result = [...list];
-    const [removed] = result.splice(sourceIndex, 1);
-    return result;
-  };
+      const result = [...list];
+      const [removed] = result.splice(sourceIndex, 1);
+      result.splice(destinationIndex, 0, removed);
+      return result;
+  }
 
   const updateCompleted = async (e) => {
-    const taskRef = doc(store, 'tasks', e.target.id);
-    await updateDoc(taskRef, {completed: e.target.checked});
-  };
+      const taskRef = doc(store, "tasks", e.target.id);
+      await updateDoc(taskRef, {completed: e.target.checked});
+  }
 
   const deleteTask = (id) => {
-    deleteDoc(doc(store, 'tasks', id));
-  };
+      deleteDoc(doc(store, "tasks", id));
+  }
 
   return (
-    <div>List</div>
+      List ?
+          <DragDropContext onDragEnd={(result => handleDrag(result))}>
+              <Droppable droppableId="task" key={List.lenght}>
+                  {(droppableProvider) =>
+                  (<div
+                      {...droppableProvider.droppableProps}
+                      ref={droppableProvider.innerRef}
+                      className="list-task">
+                      {
+                          List.map((task, id) => (
+                              <Draggable
+                                  draggableId={task.index.toString()}
+                                  index={id}
+                                  key={task.id}>
+                                  {(draggableProvider) => (
+                                      <div
+                                          {...draggableProvider.draggableProps}
+                                          ref={draggableProvider.innerRef}
+                                          {...draggableProvider.dragHandleProps}
+                                          className="task-item">
+                                          <input
+                                              type="checkbox"
+                                              id={task.id}
+                                              defaultChecked={task.completed}
+                                              onChange={updateCompleted} />
+                                          <label htmlFor={task.id}>
+                                              <BsCheck className="icon" />
+                                              <span>{task.task}</span>
+                                          </label>
+                                          <VscChromeClose 
+                                          className="icon-close"
+                                          onClick={() => deleteTask(task.id)} />
+                                      </div>
+                                  )}
+                              </Draggable>
+                          ))
+                      }
+                      {droppableProvider.placeholder}
+                  </div>)
+                  }
+              </Droppable>
+          </DragDropContext>
+          : <span>Loading...</span>
   )
 }
 
-export default List
+export default List;
