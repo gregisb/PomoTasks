6bc54a658846e9527815ae270e2a67017fd5fd59
Guilherme Regis <guilhermeb.regis@gmail.com>
2022-04-19 19:55:08 -0300

    creating list component

--- src/components/ListComponent/index.jsx
+++ src/components/ListComponent/index.jsx
@@ -1,6 +1,42 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+
+import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
+import { BsCheck } from 'react-icons/bs';
+import { VscChromeClose } from 'react-icons/vsc';
+
+import { deleteDoc, updateDoc, doc } from 'firebase/firestore';
+import store from '../../firebase/firebase.config';
 
 const List = () => {
+  const [list, setList] = useState(null);
+  const [completed, setCompleted] = useState([]);
+
+  useEffect(() => {
+    setList(list);
+  }, [list]);
+
+  const handleDrag = (result) => {
+    const { source, destination } = result;
+    if (!destination) return;
+    if (destination.index === source.index && destination.droppableId === source.droppableId) return;
+    setList(list => reorder(list, source.index, destination.index));
+  };
+
+  const reorder = (list, sourceIndex, destinationIndex) => {
+    const result = [...list];
+    const [removed] = result.splice(sourceIndex, 1);
+    return result;
+  };
+
+  const updateCompleted = async (e) => {
+    const taskRef = doc(store, 'tasks', e.target.id);
+    await updateDoc(taskRef, {completed: e.target.checked});
+  };
+
+  const deleteTask = (id) => {
+    deleteDoc(doc(store, 'tasks', id));
+  };
+
   return (
     <div>List</div>
   )
